a4fe94579b617014956b88ab0247c33d29d96832
Patrick19892209 <patrick.demel@yahoo.com>
2017-11-04 14:30:58 +0100

    testChart

---
+++ SchachingerLog/WebContent/js/bar.js
@@ -0,0 +1,58 @@
+const CHART = document.getElementById("DeliveriesPerDay");
+CHART.height = 300;
+CHART.width = 300
+let barchart = new Chart(CHART, {
+	type: 'horizontalBar',
+	data: {
+		labels: ["Monday", "Tuesday", "Wednesday", "Thirsday", "Friday"],
+		datasets: [
+			{
+				label: "Deliveries Per Day",
+				fill: true,
+				backgroundColor:[
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+					"rgba(255, 205, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(75, 50, 60, 0.2)"],
+				borderColor:[
+					"rgb(255, 99, 132)",
+					"rgb(255, 159, 64)",
+					"rgb(255, 205, 86)",
+					"rgb(75, 192, 192)",
+					"rgb(54, 162, 235)"],
+				borderWidth: 1,
+				data: [65,59,80,81,56,55,40]
+			},
+			{
+				label: "Deliveries On Time",
+				fill: true,
+				backgroundColor:[
+					"rgba(255, 99, 132, 0.5)",
+					"rgba(255, 159, 64, 0.5)",
+					"rgba(255, 205, 86, 0.5)",
+					"rgba(75, 192, 192, 0.5)",
+					"rgba(75, 50, 60, 0.5)"],
+				borderColor:[
+					"rgb(255, 99, 132)",
+					"rgb(255, 159, 64)",
+					"rgb(255, 205, 86)",
+					"rgb(75, 192, 192)",
+					"rgb(54, 162, 235)"],
+				borderWidth: 1,
+				data: [35,29,40,21,36,25,10]				
+			}
+		]
+	},
+	options: {
+		responsive: true,
+		maintainAspectRatio: false,
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}]
+		}
+	}
+});
